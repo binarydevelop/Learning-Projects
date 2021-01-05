a186031265ff45a7392b657ca1aa34c9ab1842ad
binarydevelop <binarydevelop@gmail.com>
2021-01-05 13:13:53 +0530

    Implemented Fetch function

---
+++ Profile Finder[Javascript]/main.js
@@ -0,0 +1,19 @@
+var form= document.getElementById("Inputform");
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    var search = document.getElementById('search').value;
+    console.log(search);
+ 
+    var originalName= search.split(' ').join(''); //To remove spaces
+
+    fetch("https://api.github.com/users/"+ originalName)
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+      
+      document.getElementById('result').innerHTML = ` 
+                      <a href="https://www.github.com/${originalName}">  <img src="${data.avatar_url}" /> </a>
+      `    })
+})
