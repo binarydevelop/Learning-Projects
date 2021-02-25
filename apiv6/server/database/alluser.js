44a0d77cb3307941dc58ee69ec0e15c409c00f07
binarydevelop <binarydevelop@gmail.com>
2021-02-25 11:08:03 +0530

    cleared demo db files

--- apiv6/server/database/alluser.js
+++ apiv6/server/database/alluser.js
@@ -1,25 +1,6 @@
 exports.allUser=[
    
-   {
-        "m_name": "Jon",
-        "m_power": "User"
-    },
-    {
-        "m_name": "Lannister",
-        "m_power": "User"
-    },
-    {
-        "m_name": "Hodo",
-        "m_power": "User"
-    },
-    {
-        "m_name": "John Wick",
-        "m_power": "Admin"
-    },
-    {
-        "m_name": "Flash",
-        "m_power": "Admin"
-    }
+ 
 
 ];
 
