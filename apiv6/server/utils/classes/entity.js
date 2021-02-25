4eb8e62f687f827d31a46231f5a7fb77cfbe93e8
binarydevelop <binarydevelop@gmail.com>
2021-02-25 18:54:20 +0530

    Added id count for entities

--- apiv6/server/utils/classes/entity.js
+++ apiv6/server/utils/classes/entity.js
@@ -1,4 +1,4 @@
-//get db_entity length here
+
 let id_count = 0;
 module.exports = class Entity{
     m_id;
@@ -7,9 +7,10 @@ module.exports = class Entity{
     feedback =[];
     constructor(title,category){
         this.m_title = title;
-        this.m_id = id_count++;
+        this.m_id = ++id_count;
         this.m_category = category;
         this.feedback =[];
     }
 }
+
 
