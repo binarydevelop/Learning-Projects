b367e1c9b458e4072602ab7b6e243498ebf1d75f
binarydevelop <binarydevelop@gmail.com>
2021-02-26 18:27:14 +0530

    Added UserValidation

--- apiv6/server/validation/user/user_validation.js
+++ apiv6/server/validation/user/user_validation.js
@@ -4,8 +4,7 @@ module.exports = {
     addUserValidation: async(req,res,next) => {
         const value = await user.validate(req.body)
         if(value.error) {
-            console.log(value.error)
-            res.send(value.error.details[0].message)
+            res.json(value.error.details[0].message)
         }else{
             next();
         }
